be4696f8dff267285348537f1dc9bb81820b2eb8
Ilya Kochergin .econ <koch@econ.msu.ru>
2022-05-15 09:33:02 +0300

    hint in comment to fix get_products()

--- py/data_source_ver_1.py
+++ py/data_source_ver_1.py
@@ -9,7 +9,9 @@ def get_categories(*args, **kwargs):
 
 def get_products(*args, **kwargs):
     # args and kwargs are for future project development
-    columns="productid;vendor;model;price;category".split()
+    # исправьте вызов .split() чтобы получился 
+    # список имен столбцов
+    columns="productid;vendor;model;price;category".split(";")
     yield dict(zip(columns,(13883932,"StarWind","Мини-печь smo2003",5597,5)))
     yield dict(zip(columns,(8483040,"BBK","Микроволновая печь соло 20MWS-711M/WS",4173,5)))
     yield dict(zip(columns,(65374769,"ATLANT","Холодильник Минск ХМ 4012 022",30000,6)))
